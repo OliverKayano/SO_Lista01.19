5920e0a0cdc947ed69b3180e755895cf711a5441
OliverKayano <oliver12yoshio@hotmail.com>
2026-03-25 14:47:25 -0300

    Primeira versão

---
+++ SO_lista01.19.py
@@ -0,0 +1,12 @@
+#Algoritmo SO_lista01.19.
+#Declarar.
+v1 : float = 0; 
+v2 : float = 0; 
+#Inicio.
+v1 = int(input("Insira o primeiro valor: "));
+v2 = int(input("Insira o segundo valor: "));
+if (v1>v2):
+    print ("Maior valor: ", v1);
+else:
+    print ("Maior valor: ", v2);
+#Fim.
